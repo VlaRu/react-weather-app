9726de95a52e10ba0a35584ecb79cc37550781e1
Vlada <dn210690nvs@gmail.com>
2022-12-18 19:18:38 +0100

    refactor: add rel=noreferrer in footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,9 +4,9 @@ import './Footer.css';
 export default function Footer(){
   return <div className='footer-container'>
     <p>This project was coded for 
-      <a href="https://www.shecodes.io/"  target="_blank"> SheCodes </a>
+      <a href="https://www.shecodes.io/"  target="_blank" rel="noreferrer"> SheCodes </a>
        and is open-sourced on 
-      <a href="https://github.com/VlaRu" target="_blank">
+      <a href="https://github.com/VlaRu" target="_blank" rel="noreferrer">
         <img src="https://vermillion-souffle-27bbf8.netlify.app/src/github-icon.svg" alt="github-icon" width="15" height="15"/> GitHub
       </a>
     </p>
